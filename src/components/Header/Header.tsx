6daeeed83edc23cb50020dc5f0d2040c0c3e76f1
Harley Garrett <70719407+harleygarrett@users.noreply.github.com>
2024-01-17 22:47:51 +0000

    Configure CSS reset, style Header and integrate icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,36 @@
 import Link from "next/link";
+import Image from "next/image";
+
+import styles from "./header.module.css"
 
 export default function Header() {
   return (
-    <>
+    <div className={styles.headerWrapper}>
       <a href="#main" className="sr-only" >
         Skip to content
       </a>
       
-      <header>
+      <header className={styles.header}>
 
         <Link href="/">
-          Home
+          <Image src="/icons/logo-playstation.svg" alt="Home" width={48} height={48} />
         </Link>
 
-        <nav aria-label="Account menu">
-          <ul role="list">
+        <nav aria-label="Account menu" className={styles.nav}>
+          <ul role="list" className={styles.navList}>
             <li>
-              <Link href="/messages">Messages</Link>
+              <Link href="/messages">
+                <Image src="/icons/message.svg" alt="Messages" width={32} height={32} />
+              </Link>
             </li>
             <li>
-              <Link href="/notifications">Notifications</Link>
+              <Link href="/notifications">
+                <Image src="/icons/bell.svg" alt="Notifications" width={32} height={32} />
+              </Link>
             </li>
           </ul>
         </nav>
       </header>
-    </>
+    </div>
   )
 }
