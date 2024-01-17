4e38ef3dd2e93c04e92b49eae3fcc11b70d9a55e
Harley Garrett <70719407+harleygarrett@users.noreply.github.com>
2024-01-17 22:49:44 +0000

    Style Games TabsRoot, adjust Games data

--- src/components/Games/TabsRoot.tsx
+++ src/components/Games/TabsRoot.tsx
@@ -1,7 +1,10 @@
 "use client"
 
-import Tab from "@/types/Tab"
 import * as Tabs from "@radix-ui/react-tabs"
+
+import Tab from "@/types/Tab"
+
+import styles from "./tabsRoot.module.css"
 
 interface Props {
   tabs: Tab[]
@@ -9,20 +12,20 @@ interface Props {
 
 export default function Games({tabs}: Props) {  
   return (
-    <Tabs.Root defaultValue={tabs[0].value}>
-      <Tabs.List>
+    <Tabs.Root defaultValue={tabs[0].value} className={styles.tabsRoot}>
+      <Tabs.List className={styles.tabsList}>
         {tabs.map((item, key) => 
-          <Tabs.Trigger value={item.value} key={key}>
+          <Tabs.Trigger value={item.value} key={key} className={styles.tabsTrigger}>
             {item.title}
           </Tabs.Trigger>
           )}
       </Tabs.List>
       {tabs.map((item, key) => 
         <Tabs.Content value={item.value} key={key}>
-          <ul>
+          <ul className={styles.tabpanel}>
             {item.data.map((item, key) =>
-              <li key={key}>
-                <img src={item.image} alt={item.title} />
+              <li key={key} className={styles.listItem}>
+                <img src={item.image} alt={item.title} className={styles.image} />
               </li>
             )}
           </ul>
